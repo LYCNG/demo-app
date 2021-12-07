20ccda76581ccbacf12ae8bac8fa22a67a4e84f8
fslynadmin <lyncheng@fs-technology.com>
2021-12-07 11:47:57 +0800

    dom route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 
 import './App.css';
-import {useSelector,useDispatch} from 'react-redux'
-import {bindActionCreators} from 'redux' //綁定action Creator 和dispatch
-import {actionCreators} from './state/index' // 建立action的index，寫入需要的action return {type:"",payload}
-import React from 'react';
-
+import React,{Fragment,Suspense,lazy} from 'react';
+import { Route,Routes } from "react-router-dom";
+import { HashRouter} from 'react-router-dom'
+import Home from './page/Home'
 function App() {
 
-  const  {src} = useSelector((state)=>state.logo);
-  const dispatch = useDispatch();
-  const {switchToGA,switchToGoogle} = bindActionCreators(actionCreators,dispatch);
 
   return (
-    <div className="App">
-      <h1>Nice to Meet UUU!!!</h1>
-      <img alt="" src={src} width="300" height="300"/>
-      <button onClick={switchToGA}>Switch to GA!!!</button>
-      <button onClick={switchToGoogle}>Switch to Google!!!</button>
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        {/* <Suspense fallback={<Fragment />}>
+
+        </Suspense> */}
+      </Routes>
+    </HashRouter>
   );
 }
 
---
+++ src/page/Home.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {useSelector,useDispatch} from 'react-redux'
+import {bindActionCreators} from 'redux' //綁定action Creator 和dispatch
+import {actionCreators} from '../state/index' // 建立action的index，寫入需要的action return {type:"",payload}
+
+function Home() {
+
+    const  {src} = useSelector((state)=>state.logo);
+    const dispatch = useDispatch();
+    const {switchToGA,switchToGoogle} = bindActionCreators(actionCreators,dispatch);
+    return (
+        <div className="App">
+            <h1>Nice to Meet UUU!!!</h1>
+            <img alt="" src={src} width="300" height="300"/>
+            <button onClick={switchToGA}>Switch to GA!!!</button>
+            <button onClick={switchToGoogle}>Switch to Google!!!</button>
+        </div>
+    )
+}
+
+export default Home
